27ebc90d2c7c29d26e05192bf1f9d931f0f29d36
github username <github email address>
2019-08-06 16:41:42 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const task = document.getElementById('tasks');
+  
+
+  function logSubmit(event) {
+    event.preventDefault();
+    const listTask = document.createElement("li");
+    task.appendChild(listTask);
+    listTask.textContent = event.target.children[1].value;
+  }
+
+  form.addEventListener('submit', logSubmit);
+
 });
+ 
